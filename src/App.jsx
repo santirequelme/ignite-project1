81d7ec803b7b9bf4380efe5479ad8dff9b7160fe
santirequelme <65666330+santirequelme@users.noreply.github.com>
2022-06-13 10:30:05 -0300

    sidebar upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { Post } from "./Post";
 import './global.css';
+import styles from './App.module.css';
+
 import { Header } from "./Components/Header";
+import { Sidebar} from "./Components/Sidebar";
 export function App() {
 
   return (
-    <>
-      <Header />
+    <div>
+    
+    <Header />
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
       <Post
        author="Santi"
        content="Lorem ipsum dolor"
       />
-      <Post
-       author="Pedro"
+       <Post
+       author="Santi"
        content="Lorem ipsum dolor"
       />
-    </>
+      </main>
+   
+    </div>
+    </div>
   )
 }
