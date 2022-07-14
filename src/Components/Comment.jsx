988288132b4320fd57e30aaf7f253a90adc5e2c0
santirequelme <65666330+santirequelme@users.noreply.github.com>
2022-07-14 09:50:12 -0300

    component comment styles

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,3 +1,4 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment() {
@@ -10,13 +11,19 @@ export function Comment() {
           <header>
             <div className={styles.authorAndTime}>
               <strong>Dieguito</strong>
-              <time title=''></time>
+              <time title="11 de Maio As: 08:12" dateTime="2022-11-05 08:12:30"> Ha uma hora</time>
             </div>
+            <button title="Deletar"> 
+                 <Trash size={24}/>
+              </button>
           </header>
           <p> Muito Bom Parabens</p>
         </div>
         <footer>
-          APLAUDIR
+         <button>
+          <ThumbsUp size={20}/>
+          Apaudir <span>20</span>
+         </button>
         </footer>
       </div>
     </div>
